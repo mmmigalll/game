e01e05ecfd2770858103cc5a8ad9ff6a242e9f71
mmmigalll <vashkebam1991@gmail.com>
2015-04-15 20:54:31 +0300

    add to db logic

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 var express = require('express');
-
+var mongoose = require('mongoose');
 var app = express();
 
+var db = mongoose.createConnection('mongodb://localhost/hero');
 
-require('./routes')(app);
 
-app.listen(3000, function(){
-   console.log("___Server Up successfully___");
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback() {
+
+   console.log("Connection to mainDB is success");
+
+   app.set('db', db);
+   require('./routes/index.js')(app);
+
+   app.listen(3030, function(){
+      console.log('--- Express start successful ---');
+   });
 });
+
